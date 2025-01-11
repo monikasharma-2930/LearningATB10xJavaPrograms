cef8b2b44caefbe2f0231c93b15ccb18007794f9
Monika Sharma <monika2930@gmail.com>
2025-01-11 12:36:09 +0530

    Print and Prinln difference

---
+++ src/Lec3_5Jan_JavaBasics/LAB002_print_println.java
@@ -0,0 +1,11 @@
+package Lec3_5Jan_JavaBasics;
+
+public class LAB002_print_println {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("One");
+        System.out.println("two");
+        System.out.print("three");
+        System.out.print("four");
+    }
+}
