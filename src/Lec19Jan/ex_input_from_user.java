d88af5664595b0fee18e5f558a81c5a4137cf2b7
Monika Sharma <monika2930@gmail.com>
2025-01-19 10:34:57 +0530

    Samples of Lecture - 19Jan - Input from user using command line

--- src/Lec19Jan/ex_input_from_user.java
+++ src/Lec19Jan/ex_input_from_user.java
@@ -2,7 +2,7 @@ package Lec19Jan;
 
 public class ex_input_from_user {
     public static void main(String[] args) {
-        //Take input from CLI use 'Edit Configuration'
+        //Take input from CLI use 'Edit Configuration' or java prog-name 20 10 30
 
         int age = Integer.parseInt(args[0]);
         System.out.println(age);
