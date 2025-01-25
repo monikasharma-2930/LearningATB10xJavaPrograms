6e6d9591017a93425e7b0f3a706efbc4487d01a8
Monika Sharma <monika2930@gmail.com>
2025-01-25 08:34:26 +0530

    Task 19 Jan - Question 1 -Part 1 - Find grades from Marks

--- src/Lec19Jan/Task19Jan_Part1_IdentifyGradeFromMarks.java
+++ src/Lec19Jan/Task19Jan_Part1_IdentifyGradeFromMarks.java
@@ -2,9 +2,8 @@ package Lec19Jan;
 
 public class Task19Jan_Part1_IdentifyGradeFromMarks {
     public static void main(String[] args) {
-        int Marks = 100;
-        String grade = Marks >= 90 && Marks <= 100 ? "A" : (Marks >= 80 && Marks <=90 ? "B" : (Marks >= 70 && Marks <80 ? "C" :
-                         (Marks >=60 && Marks <70 ? "D" :  "F" )));
+        int Marks = 59;
+        String grade = Marks >= 90 && Marks <= 100 ? "A" : Marks >= 80 ? "B" : Marks >= 70 ? "C" : Marks >=60 ? "D" :  "F" ;
         System.out.println(grade);
 
     }
