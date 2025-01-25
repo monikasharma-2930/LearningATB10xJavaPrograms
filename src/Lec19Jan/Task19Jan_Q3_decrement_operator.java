82983fc3c635bab8803453f0e661938c75555bd9
Monika Sharma <monika2930@gmail.com>
2025-01-25 07:42:43 +0530

    Task 19 Jan - Question 3 - Decrement Operator

---
+++ src/Lec19Jan/Task19Jan_Q3_decrement_operator.java
@@ -0,0 +1,9 @@
+package Lec19Jan;
+
+public class Task19Jan_Q3_decrement_operator {
+    public static void main(String[] args) {
+        int a = 20 ;
+        System.out.println(--a + a++ + a--);
+        System.out.println(a);
+    }
+}
