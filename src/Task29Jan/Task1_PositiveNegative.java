fa8ce7d37b38c429487494a798167ab62b6be517
Monika Sharma <monika2930@gmail.com>
2025-01-31 13:58:01 +0530

    Task#1 29Jan - Identify Positive and Negative number

--- src/Task29Jan/Task1_PositiveNegative.java
+++ src/Task29Jan/Task1_PositiveNegative.java
@@ -6,12 +6,16 @@ public class Task1_PositiveNegative {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number:");
-        int num = sc.nextInt();
-        if(num>0)
-            System.out.println("Entered number is positive");
-        else if(num<0)
-            System.out.println("Entered number is negative");
-        else
-            System.out.println("Please enter a number other than 0");
+        if(sc.hasNextInt()) {
+            int num = sc.nextInt();
+            if (num > 0)
+                System.out.println("Entered number is positive");
+            else if (num < 0)
+                System.out.println("Entered number is negative");
+            else
+                System.out.println("Please enter a number other than 0");
+        }else{
+            System.out.println("Enetered number is not integer");
+        }
     }
 }
