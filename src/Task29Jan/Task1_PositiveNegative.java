41c6a9805734c52f49e555a31e246b3743d7fb69
Monika Sharma <monika2930@gmail.com>
2025-01-31 13:21:09 +0530

    Task 25 Jan - Task 3 - Palindrome

---
+++ src/Task29Jan/Task1_PositiveNegative.java
@@ -0,0 +1,17 @@
+package Task29Jan;
+
+import java.util.Scanner;
+
+public class Task1_PositiveNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        if(num>0)
+            System.out.println("Entered number is positive");
+        else if(num<0)
+            System.out.println("Entered number is negative");
+        else
+            System.out.println("Please enter a number other than 0");
+    }
+}
