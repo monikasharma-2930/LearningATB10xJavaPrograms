0c5b0a3deafd17f7384cb428379b1561b9e66493
Monika Sharma <monika2930@gmail.com>
2025-02-04 21:31:55 +0530

    Task 15 : Check number Palindrome

--- src/Task31Jan/Task15_checkNumberPalidrome.java
+++ src/Task31Jan/Task15_checkNumberPalidrome.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class Task15_checkNumberPalidrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num;
+        int num5
         int reverseNum = 0;
         System.out.println("Enter the number:");
         if(sc.hasNextInt()){
             num = sc.nextInt();
+            int orgNum = num;
             while(num > 0){
                 reverseNum = (reverseNum*10) + (num % 10);
                 num = num/10;
             }
-            System.out.println("Reversed number: "+ reverseNum);
+            if(reverseNum == orgNum)
+                System.out.println("Entered number is a Palindrome");
+            else
+                System.out.println("Entered number is not a Palindrome");
         }else{
             System.out.println("Enter a valid integer");
         }
