d0d2ed7ed3a38b9c26469663cc13a92825053d10
Monika Sharma <monika2930@gmail.com>
2025-02-08 15:48:14 +0530

    Task 25 : identify website domain from URL

---
+++ src/Task1Feb/task25Feb_identifyWebsiteType.java
@@ -0,0 +1,48 @@
+package Task1Feb;
+
+import java.util.Scanner;
+
+public class task25Feb_identifyWebsiteType {
+    public static void main(String[] args) {
+        String url,msg;
+        String domain="";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter website URL:");
+        url = sc.nextLine();
+        if(url.trim().equals("")){
+            System.out.println("Please enter url.");
+        }else{
+           int index = url.lastIndexOf(".");
+           if(index>=0)
+               domain = url.substring(index);
+           else{
+               System.out.println("Entered URL is not valid");
+               System.exit(0);
+           }
+        }
+        switch(domain){
+            case ".com":
+                msg = "Commercial website";
+                break;
+            case ".org":
+                msg = "Non-profit organization";
+                break;
+            case ".edu":
+                msg = "Educational institution";
+                break;
+            case ".gov":
+                msg = "Government website";
+                break;
+            case ".net":
+                msg = "Network-related website";
+                break;
+            case ".info":
+                msg = " Informational website";
+                break;
+            default:
+                msg = "Unknown or other types of websites";
+                break;
+        }
+        System.out.println(msg);
+    }
+}
