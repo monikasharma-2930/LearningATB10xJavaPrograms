2fa771099bff766a0268c9bc68e4cac2b761f5d7
Monika Sharma <monika2930@gmail.com>
2025-02-09 14:36:54 +0530

    Task 1 : Print 1-10 using for and while loop

---
+++ src/Task5Feb/Task1_Print1To10.java
@@ -0,0 +1,20 @@
+package Task5Feb;
+
+public class Task1_Print1To10 {
+    public static void main(String[] args) {
+        //for loop
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+
+        //while loop
+        int j=0;
+        while(j<10){
+            j++;
+            System.out.println(j);
+        }
+
+
+    }
+
+}
