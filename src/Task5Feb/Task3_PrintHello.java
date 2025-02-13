9df9da0f957e42f7661fe73981d7366df7e10538
Monika Sharma <monika2930@gmail.com>
2025-02-13 14:23:56 +0530

    Task 3 : Print Hello 5 times using using for and while loop

---
+++ src/Task5Feb/Task3_PrintHello.java
@@ -0,0 +1,17 @@
+package Task5Feb;
+
+public class Task3_PrintHello {
+    public static void main(String[] args) {
+        System.out.println("Print using For loop:");
+        for(int i=0;i<5;i++)
+            System.out.println("Hello");
+
+        System.out.println("Print using While loop");
+        int j=0;
+        while(j<5) {
+            System.out.println("Hello");
+            j++;
+        }
+
+    }
+}
