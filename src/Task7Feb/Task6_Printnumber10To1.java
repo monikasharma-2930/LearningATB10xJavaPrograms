0575b0039fcf7cf5023c1302a8d8f144439016aa
Monika Sharma <monika2930@gmail.com>
2025-02-13 17:22:48 +0530

    Task 6 : Print numbers 1-10 in reverse order

--- src/Task7Feb/Task6_Printnumber10To1.java
+++ src/Task7Feb/Task6_Printnumber10To1.java
@@ -2,8 +2,12 @@ package Task7Feb;
 
 public class Task6_Printnumber10To1 {
     public static void main(String[] args) {
+        System.out.println("Using For loop");
         int i = 10;
         for(i=10; i>0; i--)
             System.out.println(i);
+
+        System.out.println("Using while loop");
+
     }
 }
